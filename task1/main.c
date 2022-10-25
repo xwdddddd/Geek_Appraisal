810927852a7a6a59cccfb660527a8943413fdd87
xwdddddd <1342971407@qq.com>
2022-10-25 15:33:33 +0800

    Add change1

--- task1/main.c
+++ task1/main.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	double y=0;
+	double y=2;
 	printf("enter your number:%d",y);
-	scanf("%lf",&y);
+	//scanf("%lf",&y);
 	printf("\tx\t\ty\n",y);
 	double x=0;
 	for(printf("\t%lf\t%lf\n",x,y);y-y*0.999>=0.001;y+0)
